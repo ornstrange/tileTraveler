5564c6d1465b414c8168b723496eff57be67deef
ornstrange <orn.strange@gmail.com>
2019-10-08 13:42:39 +0000

    assignment update

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,13 +1,7 @@
-# position = 1,1
-# while position is not 3,1
-#   print valid moves
-#   get input
-#   is input valid?
-#       then update position
-# print goodbye message
+from random import seed, choice
 
 def validMoves(px, py):
-    # skilar streng með valid moves eftir gildum px og py
+    # skilar streng með valid moves eftir gildum player x og player y
     if px in [1, 2, 3] and py == 1:
         return "(N)orth."
     if px == 1 and py == 2:
@@ -22,37 +16,52 @@ def validMoves(px, py):
         return "(N)orth or (S)outh."
 
 def getInput():
-    # biður um input og skilar því í hástöfum
+    # asks for a direction, returns it uppercase
     inp = input("Direction: ")
     return inp.upper()
 
 def inputValid(validMoves, inp):
-    # skilar True ef hreyfingin er leyfileg annars False
+    # returns if the input is a valid move
     validMoves = [x for x in validMoves]
     validMoves = list(filter(lambda x: 65 <= ord(x) <= 90, validMoves))
     return inp in validMoves
 
 def updatePosition(px, py, direction):
+    # updates position from direction
     py += 1 if "N" in direction else 0
     py -= 1 if "S" in direction else 0
     px += 1 if "E" in direction else 0
     px -= 1 if "W" in direction else 0
     return px, py
 
-
-posX = 1
-posY = 1
-
-print("You can travel:", validMoves(posX, posY))
 while True:
-    user_input = getInput()
-    if inputValid(validMoves(posX, posY), user_input):
-        posX, posY = updatePosition(posX, posY, user_input)
-        if posX == 3 and posY == 1:
-            print("Victory!")
-            break
+    posX = 1
+    posY = 1
+    coins = 0
+    moves = 0
+    seed_int = int(input("Input seed: "))
+    seed(seed_int)
+    while True:
+        print("You can travel:", validMoves(posX, posY))
+        #user_input = getInput()
+        user_input = choice(["N", "E", "S", "W"])
+        print(f"Direction: {user_input.lower()}")
+        moves += 1
+        if inputValid(validMoves(posX, posY), user_input):
+            posX, posY = updatePosition(posX, posY, user_input)
+            if posX == 3 and posY == 1:
+                print(f"Victory! Total coins {coins}. Moves {moves}.")
+                break
+            elif posY == 2 or (posX == 2 and posY == 3):
+                #pull_lever = input("Pull a lever (y/n): ")
+                pull_lever = choice(["Y", "N"])
+                print(f"Pull a lever (y/n): {pull_lever.lower()}")
+                if (pull_lever.upper() == "Y"):
+                    coins += 1
+                    print(f"You received 1 coin, your total is now {coins}.")
         else:
-            print("You can travel:", validMoves(posX, posY))
-    else:
-        print("Not a valid direction!")
+            print("Not a valid direction!")
+    play_again = input("Play again (y/n): ")
+    if play_again.upper() != "Y":
+        break
 
